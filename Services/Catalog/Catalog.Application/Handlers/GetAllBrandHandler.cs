f0ea32af756e0c0460e6c2521e8982a7876a03c9
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2024-11-22 12:11:13 +0400

    Creating Brand Handlers

---
+++ Services/Catalog/Catalog.Application/Handlers/GetAllBrandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetAllBrandHandler : IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
+    {
+        public IBrandRepository _brandRepository { get; }
+        public IMapper _mapper { get; }
+
+        public GetAllBrandHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+        public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+        {
+            var brandList = await _brandRepository.GetAllBrands();
+            var brandResponseList = _mapper.Map<BrandResponse>(brandList);
+            return brandResponseList;
+        }
+    }
+}
