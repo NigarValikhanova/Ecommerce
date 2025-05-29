4f7ae9ada70d4a479f084c8053c492056c89f9f4
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2025-05-29 13:46:42 +0400

    Creating Entities

---
+++ Services/Ordering/Ordering.Core/Common/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Core.Common
+{
+    public abstract class EntityBase
+    {
+        //Protected set is made to use in the derived classes
+        public int Id { get; protected set; }
+        //Below properties are Audit properties
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
